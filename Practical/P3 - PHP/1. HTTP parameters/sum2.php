19e9070c408b8d28b5d23859a339d12a4e017ceb
Pedro Macedo <pedronunomacedo@hotmail.com>
2022-05-28 23:30:09 +0100

    Started doing PHP exercises

--- Practical/P3 - PHP/1. HTTP parameters/sum2.php
+++ Practical/P3 - PHP/1. HTTP parameters/sum2.php
@@ -1,12 +1,19 @@
 <?php
-
-    $x = $_GET['x'];
-    $y = $_GET['y'];
-
-    $sum = $x + $y;
-
-    echo $x . " + " . $y . " = " . $sum;
+    $num1 = $_GET ['num1'];
+    $num2 = $_GET ['num2'];
+    $result = $num1 + $num2;
 ?>
-<br>
-<br>
-<a href="form2.html">Do another sum.</a>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sum of two numbers in PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <p><?=$num1?> + <?=$num2?> = <?=$result?></p>
+</body>
+</html>
